5001a7c696a6baaf3f196050438db2926f7ea3c9
Predret <et0interesn031@gmail.com>
2026-06-17 03:30:16 +0300

    goofy thing

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,52 @@
 use std::io::{self};
+use std::cmp::Ordering;
 
-fn main() {
-
-    let x: i32 = 5;
-
-    println!("Hello, world! {}", x);
-
-    println!("Please input something here: ");
+fn guess(number: u8) -> bool
+{
+    println!("What is your guess?\n");
     let mut input: String = String::new();
 
     io::stdin()
         .read_line(&mut input)
         .expect("Failed to read line");
+    let input_number: u8 = match input.trim().parse()
+    {
+        Ok(numb) => numb,
+        Err(_) =>
+        {
+            println!("That's not a number.");
+            return false;
+        }
+    };
+    
+    match input_number.cmp(&number)
+    {
+        Ordering::Less =>
+        {
+            println!("Your number is too low.");
+            false
+        }
+        Ordering::Greater =>
+        {
+            println!("Your number is too high.");
+            false
+        }
+        Ordering::Equal =>
+        {
+            true
+        }
+    }
+}
 
-    println!("Your input: {}", input);
+fn main() {
 
+    let number: u8 = rand::random_range(1..=100);
+    println!("{}", number);
+    let mut tries: u16 = 1;
+
+    while !guess(number) {tries += 1};
+
+    println!("Ey nice job! It took you {}", tries);
+
+    let _ = io::stdin();
 }
